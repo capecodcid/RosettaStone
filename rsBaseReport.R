6b7a0a6743ed8b5d52db355520caa5c4b7f14b63
Andrew Hartnett <a.t.hartnett@gmail.com>
2015-10-18 19:36:41 -0400

    full functioning at this point - saves to pdfs

--- rsBaseReport.R
+++ rsBaseReport.R
@@ -6,36 +6,106 @@ library(ggplot2)
 library(dplyr)
 
 # LOAD CSV FILE and Define classes
-spreadsheet = read.csv('/Users/andrewhartnett/Documents/git/RosettaStone/UsageReport-9th Grade.csv')
-testClass = c("AdebayoF172","adelekeo001","adotevim150","aguilarb103","ajuhanm002","alvesa140","wardj097","waringi117","ZabB174","zuluagas102")
+spreadsheet1  = read.csv('/Users/andrewhartnett/Downloads/UsageReport-Class of 2022.csv')
+spreadsheet2  = read.csv('/Users/andrewhartnett/Downloads/UsageReport-Class of 2023.csv')
+currentWeek = 7;
 
-caughtUp = 200;
-strikingDist = 150;
 
+grade9Goal = 250;
+grade10Goal = 500;
+weeksPerYear = 38;
+
+
+
+epstein9 = c("brisbonl017","mageea058","ajuhanm002","dekreonz027","hindsc050","padillay066","laboye093","sotoo077","wojcika134","parmentierd089","sernad086","johnsonk053","gomesc043")
+epstein10 = c("cadenahernandezm046","cardenask152","chavezs134","dasilvac180","figueroae035","ortiz-muriels142","lombak156","riverae083","maybink133")
+
+sinha9 = c("cheayee145", "cruzd123", "cruzy099", "echavarriav030", "geogheganc039", "gravierv133", "nunezn065", "peckhama143", "ramosa131", "silvierae076", "vazdopios192")
+sinha10 = c("adebayof172", "bayronriveral012", "granilloc155", "leporea054", "marroquinl060", "moralesp168", "rogersr171", "sosar166")
+
+olivo9 = c("antiguaa087", "batistal151", "dufaultj029", "dwyern083", "godoyy098", "mouratob071", "silva-milersona088", "trejor144", "vieirah096", "wilsonm100")
+olivo10 = c("arboledaj004", "ariasl007", "bravog078", "cassianik020", "encarnaciona179", "feldb033", "gallegosgomezm139", "lopesn057", "ricaurteg115", "tebaldih170", "trejoa092", "vasquezj175")
+
+delanos9 = c("gomezn044", "chavezk023", "moored137", "ayalab008", "riganom071", "kinnesl057", "jadachp047", "lemuso118", "restrepoa070")
+delanos10 = c("colonias109", "zabb174", "adotevim150", "zuluagas102", "velezm124", "adelekeo001", "girardt043", "aguilarb103", "gomese173", "tavaresa165")
+
+caraza9 = c("agudeloj001","araujog006","armans008", "beaudoinh011","doughtyl027","geraldesj040","santoss073","silvam075","thorntons114")
+caraza10 = c("seebeckm128","armans008","marrerod059","marshs061","nyantig130","pinedae078","rodriguezv084","stagerj089","wardj097","waringi117")
+
+rupani9 = c("alvarados004",  "contrerasc019", "curryd021",  "garciaa094", "lucarioj122", "marksa109",  "quianesj068", "ramosf081", "villac085", "godineze091")
+rupani10 = c("alvesa140", "corream026", "pereirak135", "ramirezb143", "riganoe137", "trinidads176", "varoner094", "torresa091")
+
+allcock9 = c("belloj136" , "deveigae023" , "davisd140" , "dejesuspereza025" , "fontalvoc038" , "gamboad121" , "hernandeza136" , "jovinc132" , "maganad116" , "morenoj063" , "rojasj113", "vincented120")
+allcock10 = c("ardoncantef005", "baross011", "deighanh154" , "diaze031" , "lewise055" , "mingoesc070" , "pinak160" , "tavaresn163")
+
+gibson9 = c("delgadot026","feleyt031","gibbonss041","godineza042","alvarezj084","guzmand095","morine110","wojcikj138","lamoureuxe145")
+gibson10 = c("cardonaa019","choquettet024","flanagank037","lincolns056","mckeec065","mensahb069","muneragoezm072","rourket085","valderramal093","malikt112","garcial126")
+
+payette9 = c("dasilvaj022","donisd028","fuentesd036","hernandezc102","khanz126","mejiaa061","millettes088","onifadef142","urzuac082","veigat119")
+payette10 = c("castilloj021","cortesm027","costad129","fernandess136","henrriquezw049","mckeej066","melloj113","phillipsd177","sweeneyj090","vasquezj116", "vicentem096", "wieganda099")
+
+classes9 = list(epstein=epstein9,sinha=sinha9,olivo=olivo9,delanos=delanos9,caraza=caraza9,rupani=rupani9,allcock=allcock9,gibson=gibson9,payette=payette9)
+classes10 = list(epstein=epstein10,sinha=sinha10,olivo=olivo10,delanos=delanos10,caraza=caraza10,rupani=rupani10,allcock=allcock10,gibson=gibson10,payette=payette10)
+
+
+expect9 = currentWeek*(grade9Goal/weeksPerYear);
+expect10 = currentWeek*((grade10Goal-200)/weeksPerYear)+200;
+
+strikingDist = 20;
 
 #
 # Definitions of Functions and Styles
 #
+
 colorPalette = c('#3FDB35','#FFED1B','#FF1605')
 
-getStatus <- function(progress, greenLine, yellowLine) {
-  if (progress >= greenLine) {
-    status <- "okay"
-  } else if (progress >= yellowLine) {
-    status <- "warning"
-  } else {
-    status <- "danger"
+getStatus <- function(progress, grade) {
+  if (grade==9){
+    if (progress >= expect9) {
+      status <- "okay"
+    } else if (progress >= expect9-strikingDist) {
+      status <- "warning"
+    } else {
+      status <- "danger"
+    }
+  }
+  else if (grade==10){
+    if (progress >= expect10) {
+      status <- "okay"
+    } else if (progress >= expect10-strikingDist) {
+      status <- "warning"
+    } else {
+      status <- "danger"
+    }
   }
   return(status)
 }
 
-
-
+findClass <- function(usr) {
+  usr = as.character(usr)
+  usr = tolower(usr)
+  for (cl in names(classes10)){
+    print
+    if (tolower(usr) %in% classes10[[cl]]) {
+      return(cl)
+    }
+    else if (tolower(usr) %in% classes9[[cl]]) {
+      return(cl)
+    }
+  }
+  return('NoClass')
+}
 
 # REMOVE FIRST ROW AND ALL UNNEEDED COLUMNS
 # keeping username, language, % completion, hours
 # set up correct names and make sure all columns are the correct class
-dat = spreadsheet[2:nrow(spreadsheet),c(3,6,10,12)]
+dat1 = spreadsheet1[2:nrow(spreadsheet1),c(3,6,10,12)]
+dat2 = spreadsheet2[2:nrow(spreadsheet2),c(3,6,10,12)]
+dat1$grade = '10'
+dat2$grade = '9'
+
+dat <- rbind(dat1, dat2)
+
 dat<-rename(dat,user=X.1)
 dat<-rename(dat,lang=X.4)
 dat<-rename(dat,percent=X.8)
@@ -43,31 +113,37 @@ dat$percent <- as.numeric(as.character(dat$percent))
 dat<-rename(dat,hours=X.10)
 dat$hours<-as.numeric(as.character(dat$hours))
 rownames(dat)<-1:nrow(dat)
+dat$grade<-as.factor(dat$grade)
 
 
 # Group By user and calculate total progress and time spent
-byUser <- group_by(dat, user)
-userTotals<-summarise(byUser,totalProgress=sum(percent), totalTime=sum(hours)) %>% rowwise() %>% mutate(status = getStatus(totalProgress,caughtUp,strikingDist))
-userTotals$status=factor(userTotals$status, levels=c("okay","warning","danger"))
+byUser <- group_by(dat, user, grade)
+userTotals<-summarise(byUser,totalProgress=sum(percent), totalTime=sum(hours)) 
 
-# seperate classes
-tC1 <- userTotals %>% 
-  filter(user %in% testClass) %>%
-  arrange(totalProgress)
+for (cnt in 1:dim(userTotals)[1]) {
+  #userTotals$grade[cnt] <- findGrade(userTotals$user[cnt])
+  userTotals$proctor[cnt] <- findClass(userTotals$user[cnt])
+}
+userTotals$proctor <- as.factor(userTotals$proctor)
 
+userTotals = userTotals %>% rowwise() %>% mutate(status = getStatus(totalProgress,grade))
+userTotals$status= factor(userTotals$status, levels=c("okay","warning","danger"))
 
-# bar plot
-classPlot <- function(dfClass) {
+testPlot <- function(dfClass, expect9, expect10, grade9Goal, grade10Goal) {
   plot <- ggplot(dfClass, aes(x=reorder(user,totalProgress), y=totalProgress, fill=status))
-  plot <- plot + geom_bar(stat="identity") + coord_flip() + scale_fill_manual(values=colorPalette) + ylab("Total Progress to Date") + xlab("Username")
-  return(plot)
+  plot <- plot + ggtitle(first(dfClass$proctor)) + geom_bar(stat="identity") + coord_flip() + 
+    scale_fill_manual(values=colorPalette,limits = levels(dfClass$status)) + 
+    ylab("Total Progress to Date") + xlab("Username") 
+  
+  if (first(dfClass$grade) == 9){
+    plot <- plot + ylim(0,grade9Goal) + geom_hline(aes(yintercept=expect9))
+  }
+  if (first(dfClass$grade)==10){
+    plot <- plot + ylim(0,grade10Goal) + geom_hline(aes(yintercept=expect10))
+  }
+  fileName = paste0(first(dfClass$proctor),'_',first(dfClass$grade),'_week_',currentWeek,'.pdf');
+  ggsave(filename = fileName, plot = plot, width = 7, height = 5)
 }
+group_by(userTotals, proctor, grade) %>%
+  do(plot = testPlot(.,expect9, expect10, grade9Goal, grade10Goal))
 
-adminPlot <- function(dfAllUsers) {
-  plot <- ggplot(dfAllUsers, aes(x=reorder(user,totalProgress), y=totalProgress, fill=status))
-  plot <- plot + geom_bar(stat="identity") + coord_flip() + scale_fill_manual(values=colorPalette) + ylab("Total Progress to Date") + xlab("Username")
-  return(plot)
-}
-
-classPlot(tC1)
-adminPlot(userTotals)
